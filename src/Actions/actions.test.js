2b76cc2388dc114789f17cea95a4c8e2febee4ba
Ricardo Viera <ricardoclean@gmail.com>
2018-04-08 08:17:04 -0600

    Creates addFitBitData and addHeartRate tests and actioncreators

--- src/Actions/actions.test.js
+++ src/Actions/actions.test.js
@@ -11,3 +11,27 @@ describe('addUser', () => {
     expect(result).toEqual(expected);
   });
 });
+
+describe('addFitBitData', () => {
+  it('should create addFitBitData action object', () => {
+    const mockFitBitUser = 'Ricardo V.';
+    const expected = {
+      type: 'ADD_FITBIT_USER',
+      user: mockFitBitUser
+    };
+    const result = Actions.addFitBitUser(mockFitBitUser);
+    expect(result).toEqual(expected);
+  });
+});
+
+describe('addHeartRate', () => {
+  it('should create addHeartRate action object', () => {
+    const mockHeartRate = 65;
+    const expected = {
+      type: 'ADD_HEART_RATE',
+      heartRate: mockHeartRate
+    };
+    const result = Actions.addHeartRate(mockHeartRate);
+    expect(result).toEqual(expected);
+  });
+});
--- src/Actions/index.js
+++ src/Actions/index.js
@@ -2,3 +2,13 @@ export const addUser = user => ({
   type: 'ADD_USER',
   user
 });
+
+export const addFitBitUser = user => ({
+  type: 'ADD_FITBIT_USER',
+  user
+});
+
+export const addHeartRate = heartRate => ({
+  type: 'ADD_HEART_RATE',
+  heartRate
+});
