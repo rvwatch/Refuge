0c80e181f2a20fb02ea7a31604cc03f6decab75e
Ricardo Viera <ricardoclean@gmail.com>
2018-04-08 08:01:14 -0600

    Creates addUser action and test

--- src/Actions/actions.test.js
+++ src/Actions/actions.test.js
@@ -1,1 +1,13 @@
 import * as Actions from './index';
+
+describe('addUser', () => {
+  it('should create addUser action object', () => {
+    const mockUser = 'Ricardo';
+    const expected = {
+      type: 'ADD_USER',
+      user: mockUser
+    };
+    const result = Actions.addUser(mockUser);
+    expect(result).toEqual(expected);
+  });
+});
