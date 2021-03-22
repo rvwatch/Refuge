369b391fbc9cf45210561203d7a6251002544655
rvwatch <astro.viera@gmail.com>
2021-03-22 09:30:21 -0600

    attempting to update routs

--- src/Containers/LoginContainer/SignIn/newSignIn.js
+++ src/Containers/LoginContainer/SignIn/newSignIn.js
@@ -1,62 +1,69 @@
 import React, { useEffect, useState } from 'react';
-import firebase from 'firebase';
+// import firebase from 'firebase';
 import '../LoginContainer.css';
-import { postLogin } from '../../../ApiCalls/postLogin';
-import { useDispatch } from 'react-redux';
-import * as Actions from '../../../Actions/';
+// import { postLogin } from '../../../ApiCalls/postLogin';
+// import { useDispatch } from 'react-redux';
+import { useHistory } from "react-router-dom";
+// import * as Actions from '../../../Actions/';
 
+const NewSignIn = () => {
+  // useEffect( () => {
+  //   firebase.initializeApp({
+  //     apiKey: 'AIzaSyAcxCaic7ghfcSvxLbWeKUed3nmFNXaiME',
+  //     authDomain: 'refuge-d2418.firebaseapp.com',
+  //     projectId: 'refuge-d2418',
+  //     storageBucket: 'refuge-d2418.appspot.com',
+  //     messagingSenderId: '115368666273',
+  //     appId: '1:115368666273:web:a7e840b2a34527ee9e3ce0',
+  //     measurementId: 'G-S48FW6R2BT'
+  //   });
+  // }, []);
+  let history = useHistory();
 
+  const [input, setInput] = useState({});
+  const [loggedIn, setLoggedIn] = useState(false);
 
-function NewSignIn (){
+  const handleInputChange = (e) => 
+  setInput({
+    ...input,
+    [e.currentTarget.name]: e.currentTarget.value,
+  });
+
+  const signInUser = async event => {
+    event.preventDefault();
+    const { username, password } = input;
+    console.log(username);
+    console.log(password);
+    setLoggedIn(current => !current);
+    console.log(loggedIn);
+    history.push('/');
+    // const user = await postLogin(username, password);
+    // this.props.addUser(user);
+    // this.props.setLoggedIn();
+    // this.props.history.push('/');
+  };
 
   useEffect( () => {
-    firebase.initializeApp({
-      apiKey: 'AIzaSyAcxCaic7ghfcSvxLbWeKUed3nmFNXaiME',
-      authDomain: 'refuge-d2418.firebaseapp.com',
-      projectId: 'refuge-d2418',
-      storageBucket: 'refuge-d2418.appspot.com',
-      messagingSenderId: '115368666273',
-      appId: '1:115368666273:web:a7e840b2a34527ee9e3ce0',
-      measurementId: 'G-S48FW6R2BT'
-    });
-  }, []);
-
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
-  //   const dispatch = useDispatch(); 
-  const signInUser = (event) => {
-    event.preventDefault();
-    const { name, value } = event.target;
-    console.log('====================================');
-    console.log({[name]: value});
-    console.log('====================================');
-
-    // const { username, password } = event.target;
-    setUsername({[name]: value});
-    // setPassword(password.value);
-    // dispatch(Actions.addUser(user));
-  };
+    console.log(loggedIn);
+}, [loggedIn]);
 
   return (
     <form className="signin-form" onSubmit={signInUser}>
       <h2>Sign-In</h2>
       <div>
         <input
-          onChange={signInUser}
+          onChange={handleInputChange}
           type="text"
           placeholder="email"
           name="username"
-          value={username}
         />
       </div>
       <div>
         <input
-          onChange={signInUser}
+          onChange={handleInputChange}
           type="password"
           placeholder="password"
           name="password"
-          value={password}
         />
       </div>
       <div>
@@ -64,8 +71,5 @@ function NewSignIn (){
       </div>
     </form>
   );
-}
-
-
-
+};
 export default NewSignIn;
